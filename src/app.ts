d523715d0f22090a06fb8c5cff4f7a359b13a561
GabrielGalati <gabrielgalati.js@gmail.com>
2020-08-06 18:46:03 -0400

    dev

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { ProductResolver } from "./resolvers/ProductResolver";
+
+export async function startServer() {
+  const app = express();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ProductResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  return app;
+}
